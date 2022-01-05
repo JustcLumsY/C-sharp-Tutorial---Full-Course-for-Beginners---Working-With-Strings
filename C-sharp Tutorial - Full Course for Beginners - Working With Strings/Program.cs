edc47567bcc685c4a5634498e798e1c1117cf08a
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 11:20:14 +0100

    Add project files.

---
+++ C-sharp Tutorial - Full Course for Beginners - Working With Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Full_Course_for_Beginners___Working_With_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Get Academy";
+
+            Console.WriteLine(phrase.Substring(phrase.IndexOf("Academy")) );
+
+            Console.ReadLine();
+        }
+    }
+}
